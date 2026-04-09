7c06b2ad9024dcca0cbaeeeb740538b61e635c5e
oncusener <oncu.sener@gmail.com>
2026-04-09 11:53:39 +0300

    yeni tasarım ve arka plan+

--- constants/sounds.ts
+++ constants/sounds.ts
@@ -1,24 +1,10 @@
 export const SOUNDS = [
     {
-        id: 'ambient',
-        name: 'Jungle',
+        id: 'forest',
+        name: 'Forest',
         premium: false,
-        file: require('../assets/sounds/ambient.mp3'),
-        image: require('../assets/pics/ambient.png')
-    },
-    {
-        id: 'bass',
-        name: 'Bass',
-        premium: false,
-        file: require('../assets/sounds/bass.mp3'),
-        image: require('../assets/pics/bass.png')
-    },
-    {
-        id: 'rain',
-        name: 'Rain',
-        premium: false,
-        file: require('../assets/sounds/rain.mp3'),
-        image: require('../assets/pics/rain.png')
+        file: require('../assets/sounds/forest.mp3'),
+        image: require('../assets/pics/forest.png')
     },
     {
         id: 'ocean',
@@ -28,34 +14,6 @@ export const SOUNDS = [
         image: require('../assets/pics/ocean.png')
     },
     {
-        id: 'wind',
-        name: 'Wind',
-        premium: false,
-        file: require('../assets/sounds/wind.mp3'),
-        image: require('../assets/pics/wind.png')
-    },
-    {
-        id: 'fire',
-        name: 'Fireplace',
-        premium: false,
-        file: require('../assets/sounds/fire.mp3'),
-        image: require('../assets/pics/fire.png')
-    },
-    {
-        id: 'forest',
-        name: 'Forest',
-        premium: false,
-        file: require('../assets/sounds/forest.mp3'),
-        image: require('../assets/pics/forest.png')
-    },
-    // {
-    //     id: 'brownnoise',
-    //     name: 'Brown Noise',
-    //     premium: false,
-    //     file: require('../assets/sounds/brownnoise.wav'),
-    //     image: require('../assets/pics/brownnoise.png')
-    // },
-    {
         id: 'river',
         name: 'River',
         premium: false,
@@ -63,37 +21,82 @@ export const SOUNDS = [
         image: require('../assets/pics/river.png')
     },
     {
+        id: 'ambient',
+        name: 'Jungle',
+        premium: true,
+        file: require('../assets/sounds/ambient.mp3'),
+        image: require('../assets/pics/ambient.png')
+    },
+    {
+        id: 'bass',
+        name: 'Bass',
+        premium: true,
+        file: require('../assets/sounds/bass.mp3'),
+        image: require('../assets/pics/bass.png')
+    },
+    {
+        id: 'rain',
+        name: 'Rain',
+        premium: true,
+        file: require('../assets/sounds/rain.mp3'),
+        image: require('../assets/pics/rain.png')
+    },
+
+    {
+        id: 'wind',
+        name: 'Wind',
+        premium: true,
+        file: require('../assets/sounds/wind.mp3'),
+        image: require('../assets/pics/wind.png')
+    },
+    {
+        id: 'fire',
+        name: 'Fireplace',
+        premium: true,
+        file: require('../assets/sounds/fire.mp3'),
+        image: require('../assets/pics/fire.png')
+    },
+
+    // {
+    //     id: 'brownnoise',
+    //     name: 'Brown Noise',
+    //     premium: false,
+    //     file: require('../assets/sounds/brownnoise.wav'),
+    //     image: require('../assets/pics/brownnoise.png')
+    // },
+
+    {
         id: 'thunder',
         name: 'Thunder',
-        premium: false,
+        premium: true,
         file: require('../assets/sounds/thunder.mp3'),
         image: require('../assets/pics/thunder.png')
     },
     {
         id: 'cafe',
         name: 'Café',
-        premium: false,
+        premium: true,
         file: require('../assets/sounds/cafe.mp3'),
         image: require('../assets/pics/cafe.png')
     },
     {
         id: 'bus_noise',
         name: 'Bus noise',
-        premium: false,
+        premium: true,
         file: require('../assets/sounds/bus_noise.mp3'),
         image: require('../assets/pics/bus_noise.png')
     },
     {
         id: 'baby_sleeping',
         name: 'Baby Sleep',
-        premium: false,
+        premium: true,
         file: require('../assets/sounds/baby_sleeping.mp3'),
         image: require('../assets/pics/baby_sleeping.png')
     },
     {
         id: 'street',
         name: 'Street',
-        premium: false,
+        premium: true,
         file: require('../assets/sounds/street.mp3'),
         image: require('../assets/pics/street.png')
 
